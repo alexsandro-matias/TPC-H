e8807ce97af345fbb98a7b197041c148268ab2ea
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2019-09-23 23:15:05 -0300

    adicionado o arquivo de pós instalação do Fedora RawHide

---
+++ posInstalacaoFedora.sh
@@ -0,0 +1,10 @@
+sudo dnf update
+sudo dnf upgrade
+sudo dnf install neofetch redshift-gtk
+sudo dnf install google-chrome-unstable-78.0.3904.17-1.x86_64 git telegram-desktop
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install vlc
+sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
+sudo dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
+sudo dnf install vlc ark  -y  
